2b3b2f29863eb356cd26b0f1e11f568e8a134149
manitpong-prog <manit.pong@gmail.com>
2026-05-27 19:23:11 +0700

    Add Supabase Retrofit service factory

---
+++ app/src/main/java/com/example/sync/SupabaseService.kt
@@ -0,0 +1,42 @@
+package com.example.sync
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object SupabaseService {
+    private val moshi: Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit: Retrofit? by lazy {
+        if (!SupabaseConfig.isConfigured) {
+            null
+        } else {
+            Retrofit.Builder()
+                .baseUrl("${SupabaseConfig.url}/")
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+        }
+    }
+
+    val authApi: SupabaseAuthApi?
+        get() = retrofit?.create(SupabaseAuthApi::class.java)
+
+    val notesApi: SupabaseNotesApi?
+        get() = retrofit?.create(SupabaseNotesApi::class.java)
+
+    fun bearer(accessToken: String): String = "Bearer $accessToken"
+}
