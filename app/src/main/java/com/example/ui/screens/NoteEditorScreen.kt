47675349920be2b14c89a4147fbdc2463bdc0062
manitpong-prog <manit.pong@gmail.com>
2026-05-27 21:53:03 +0700

    Import verticalScroll for read mode

--- app/src/main/java/com/example/ui/screens/NoteEditorScreen.kt
+++ app/src/main/java/com/example/ui/screens/NoteEditorScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
