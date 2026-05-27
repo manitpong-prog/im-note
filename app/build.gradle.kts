8f4189b7209558c917f875299e49256333692295
manitpong-prog <manit.pong@gmail.com>
2026-05-27 18:01:33 +0700

    Change app id to com.imnote.app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.imnote.app"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.manitpong.imnote"
+    applicationId = "com.imnote.app"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
