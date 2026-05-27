1df41f717a2a2e72f4f05eaa53de7ca0483ad80a
manitpong-prog <manit.pong@gmail.com>
2026-05-27 18:03:01 +0700

    Update test assertion for com.imnote.app

--- app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ExampleInstrumentedTest.kt
@@ -14,6 +14,6 @@ class ExampleInstrumentedTest {
   @Test
   fun useAppContext() {
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.manitpong.imnote", appContext.packageName)
+    assertEquals("com.imnote.app", appContext.packageName)
   }
 }
