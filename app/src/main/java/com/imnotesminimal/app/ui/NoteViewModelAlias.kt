f69519c6f58f6103fe9ee1705dacba14509559a3
manitpong-prog <manit.pong@gmail.com>
2026-05-28 06:58:10 +0700

    Add app package aliases for ui viewmodel

---
+++ app/src/main/java/com/imnotesminimal/app/ui/NoteViewModelAlias.kt
@@ -0,0 +1,5 @@
+package com.imnotesminimal.app.ui
+
+typealias NoteViewModel = com.example.ui.NoteViewModel
+typealias SortMode = com.example.ui.SortMode
+typealias SearchMode = com.example.ui.SearchMode
