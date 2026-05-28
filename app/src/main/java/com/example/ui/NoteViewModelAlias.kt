e09e829ccf69a2fa2a1abe04d5909675aa753fe9
manitpong-prog <manit.pong@gmail.com>
2026-05-28 07:15:42 +0700

    Remove duplicate UI alias file

--- app/src/main/java/com/example/ui/NoteViewModelAlias.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ui
-
-typealias NoteViewModel = com.imnotesminimal.app.ui.NoteViewModel
-typealias SortMode = com.imnotesminimal.app.ui.SortMode
-typealias SearchMode = com.imnotesminimal.app.ui.SearchMode
