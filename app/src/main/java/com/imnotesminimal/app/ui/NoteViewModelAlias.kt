04cb5da298428735b4933d49866244dabd97eda3
manitpong-prog <manit.pong@gmail.com>
2026-05-28 07:05:32 +0700

    Remove incorrect app package viewmodel alias

--- app/src/main/java/com/imnotesminimal/app/ui/NoteViewModelAlias.kt
+++
@@ -1,5 +0,0 @@
-package com.imnotesminimal.app.ui
-
-typealias NoteViewModel = com.example.ui.NoteViewModel
-typealias SortMode = com.example.ui.SortMode
-typealias SearchMode = com.example.ui.SearchMode
